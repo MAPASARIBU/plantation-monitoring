20811192cf1482c1dba2c34993312a31ad87da05
MUHAMMAD ALI PASARIBU <Alipasaribu873@gmail.com>
2026-07-10 00:16:24 +0700

    Update Liquid hours to start from 07:00 every 1 hour

--- mill_modules.js
+++ mill_modules.js
@@ -73,12 +73,18 @@ views.processing = `
             <div style="display: flex; gap: 15px; margin-bottom: 20px;">
                 <input type="date" id="ml-date" class="form-control" style="width: 150px;" onchange="loadLiquidHour()">
                 <select id="ml-hour" class="form-control" style="width: 120px;" onchange="loadLiquidHour()">
-                    <option value="06:00">06:00</option><option value="08:00">08:00</option>
-                    <option value="10:00">10:00</option><option value="12:00">12:00</option>
-                    <option value="14:00">14:00</option><option value="16:00">16:00</option>
-                    <option value="18:00">18:00</option><option value="20:00">20:00</option>
-                    <option value="22:00">22:00</option><option value="24:00">24:00</option>
-                    <option value="02:00">02:00</option><option value="04:00">04:00</option>
+                    <option value="07:00">07:00</option><option value="08:00">08:00</option>
+                    <option value="09:00">09:00</option><option value="10:00">10:00</option>
+                    <option value="11:00">11:00</option><option value="12:00">12:00</option>
+                    <option value="13:00">13:00</option><option value="14:00">14:00</option>
+                    <option value="15:00">15:00</option><option value="16:00">16:00</option>
+                    <option value="17:00">17:00</option><option value="18:00">18:00</option>
+                    <option value="19:00">19:00</option><option value="20:00">20:00</option>
+                    <option value="21:00">21:00</option><option value="22:00">22:00</option>
+                    <option value="23:00">23:00</option><option value="24:00">24:00</option>
+                    <option value="01:00">01:00</option><option value="02:00">02:00</option>
+                    <option value="03:00">03:00</option><option value="04:00">04:00</option>
+                    <option value="05:00">05:00</option><option value="06:00">06:00</option>
                 </select>
             </div>
             <!-- inputs table for Liquid -->
@@ -577,7 +583,7 @@ function updateProcessingHistorical() {
     const lBody = document.querySelector('#hist-liquid-table tbody');
     const fBody = document.querySelector('#hist-ffa-table tbody');
     
-    const hoursL = ['08:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00','24:00','02:00','04:00','06:00'];
+    const hoursL = ['07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00','24:00','01:00','02:00','03:00','04:00','05:00','06:00'];
     let hL = '';
     hoursL.forEach(h => {
         let row = window.currentLiquidData.find(d => d.time_hour === h) || {};
